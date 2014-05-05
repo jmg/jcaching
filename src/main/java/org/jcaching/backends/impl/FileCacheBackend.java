712d31412996906cdb87f522120cd41ad38a7975
Ariel Gerardo Ríos <ariel.gerardo.rios@gmail.com>
2014-05-05 15:29:23 -0300

    Add test case declartion.

--- src/main/java/org/jcaching/backends/impl/FileCacheBackend.java
+++ src/main/java/org/jcaching/backends/impl/FileCacheBackend.java
@@ -4,6 +4,56 @@
  * TODO Description if available.
  */
 
-// TODO code here
+package org.jcaching.backends.impl;
+
+import org.jcaching.backends.CacheBackend;
+
+/**
+ * TODO
+ */
+class FileCacheBackend implements CacheBackend {
+
+    /**
+     * Default constructor.
+     */
+    public FileCacheBackend() {
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean set(String key, String serializedObj) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean set(String key, String serializedObj, int timeout) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String get(String key) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean delete(String key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}
 
 // vim:ft=java ts=4 tw=80 cc=+1: 
---
+++ src/test/java/org/jcaching/backends/impl/FileCacheBackendTestCase.java
@@ -0,0 +1,41 @@
+/**
+ * FileCacheBackendTestCase
+ *
+ * TODO Description if available.
+ */
+
+package org.jcaching.backends.impl;
+
+import junit.framework.TestCase;
+
+public class FileCacheBackendTestCase extends TestCase {
+
+    /**
+     * Default constructor.
+     */
+    public FileCacheBackendTestCase() {
+        super();
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see TestCase#setUp()
+     */
+    public void setUp() {
+    }
+
+    /**
+     * {@inheritDoc}
+     * @see TestCase#tearDown()
+     */
+    public void tearDown() {
+    }
+
+    /**
+     * TODO
+     */
+    public void testSetOk() {
+    }
+}
+
+// vim:ft=java ts=4 tw=80 cc=+1: 
