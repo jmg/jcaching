76591728eb9b7847923c274c771b5868678dc26a
Ramiro Machado <machado.ramiromiguel@gmail.com>
2014-05-16 00:25:14 -0300

    Serializados los atributos, incluso los privados

--- src/main/java/org/jcaching/serializer/Serializer.java
+++ src/main/java/org/jcaching/serializer/Serializer.java
@@ -4,19 +4,29 @@ import java.lang.reflect.Field;
 
 public class Serializer {
 
-	public String serialize(Object serializableObject) throws ClassNotFoundException {
+	public String serialize(Object serializableObject){
 		String finalString ="{ ";
+		Class<?> clazz = serializableObject.getClass();
 		
 		//Save the name of the class
-		String objectClass = serializableObject.getClass().toString();
-		finalString+="Class:"+objectClass+"";
+		String objectClass = clazz.getName();
+		finalString+="Class:"+objectClass+"\n";
 		
 		//Sabe the attr
-		Class<?> clazz = Class.forName(objectClass);
-		Field attrs[] = clazz.getDeclaredFields(); //Que onda?
+		Field attrs[] = clazz.getDeclaredFields(); //Que onda? http://docs.oracle.com/javase/tutorial/reflect/class/classMembers.html
 		
-		finalString +="\n}";
+		
+		for( Field attr : attrs){
+			boolean accessible = attr.isAccessible();
+			attr.setAccessible(true);
+			try {finalString += attr.getType()+" "+attr.getName() +":"+attr.get(serializableObject)+"\n" ;} catch(IllegalAccessException e){}
+			attr.setAccessible(accessible);
+		}
+		
+		finalString +="}";
 		return finalString;
 	}
+	
+	
 
 }
